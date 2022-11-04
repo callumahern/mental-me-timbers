64dc6db64ba2f3dae74707d1a49fea55424c13fe
callumahern <callum.ahern@simplybusiness.co.uk>
2022-11-04 12:47:02 +0000

    food element added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def homepage 
+    @food = Food.all
     @entry = Entry.where(
       start_time: DateTime.now.beginning_of_month.beginning_of_week..DateTime.now.end_of_month.end_of_week
     )
